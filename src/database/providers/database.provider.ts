4ceddbc20744f1aca1e22d9056f9b2139f13c396
Joel Campos <joel.campos@primer-impacto.com>
2023-10-23 14:54:41 +0200

    Use env

--- src/database/providers/database.provider.ts
+++ src/database/providers/database.provider.ts
@@ -1,16 +1,21 @@
-import { Sequelize } from 'sequelize-typescript';
+import { Sequelize } from "sequelize-typescript";
+import { getEnv } from "../../config/env";
 
 export const databaseProviders = [
   {
-    provide: 'SEQUELIZE',
+    provide: "SEQUELIZE",
     useFactory: async () => {
+      const {
+        database: { host, username, password, name: database, port },
+      } = getEnv();
+
       const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'password',
-        database: 'nest',
+        dialect: "mysql",
+        host,
+        port,
+        username,
+        password,
+        database,
       });
       sequelize.addModels([]);
       await sequelize.sync();
